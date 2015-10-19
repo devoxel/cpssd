41819cd2a89d21523bc76dfe5580071afd81abfe
Aaron Delaney <devoxel@gmail.com>
2015-10-20 00:35:20 +0100

    Add text input and info view

--- spellcheck/dist/main.js
+++ spellcheck/dist/main.js
@@ -11,15 +11,61 @@ you'll be able to find it at my github!
 
  * Dependincies
 
-- jQuery, to make dom manipulations work across all browsers
+- jQuery, to make DOM manipulations work across all browsers
  */
 
 (function() {
-  var Config, EditorController, EditorModel, EditorView, config, editor_controller, editor_model, editor_view;
+  var Config, EditorController, EditorModel, EditorView, check_spelling, config, countStr, editor_controller, editor_model, editor_view, format_misspelling,
+    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+  countStr = function(string, regex) {
+    var count, i, len, ref, word;
+    count = 0;
+    ref = string.split(regex);
+    for (i = 0, len = ref.length; i < len; i++) {
+      word = ref[i];
+      if (word.length > 0) {
+        count += 1;
+      }
+    }
+    return count;
+  };
+
+  format_misspelling = function(l) {
+    var i, len, other_spellings, s, spelling, word;
+    console.log(l);
+    s = "";
+    for (word in l) {
+      other_spellings = l[word];
+      s += word + "<br>";
+      for (i = 0, len = other_spellings.length; i < len; i++) {
+        spelling = other_spellings[i];
+        s += "? " + spelling + "<br>";
+      }
+    }
+    return s;
+  };
+
+  check_spelling = function(string, word_regex, word_list) {
+    var i, len, misspelled, ref, ref1, word;
+    misspelled = {};
+    console.log(string.split(word_regex));
+    ref = string.split(word_regex);
+    for (i = 0, len = ref.length; i < len; i++) {
+      word = ref[i];
+      if (word.length > 0 && (ref1 = word.toLowerCase(), indexOf.call(word_list, ref1) < 0)) {
+        misspelled[word] = ['put reccomendations here'];
+      }
+    }
+    return misspelled;
+  };
 
   Config = (function() {
     function Config() {
       this.debug = true;
+      this.welcome_text = "Welcome to wand";
+      this.word_regex = /[\ ,\.\!\;\?]|<br>/;
+      this.word_list = ["the", "and", "cat", "welcome", "to", "wand"];
     }
 
     return Config;
@@ -33,8 +79,58 @@ you'll be able to find it at my github!
       if (this.config.debug) {
         console.log("+ Iniating Model");
       }
-      this.text = "Welcome to wand.";
+      this.text = this.config.welcome_text;
+      this.initContainer();
+      this.initTextArea();
+      this.initInfoArea();
+      this.updateWindowSize();
     }
+
+    EditorModel.prototype.initContainer = function() {
+      $('body').append('<div id="container"></div>');
+      return this.container = $('#container');
+    };
+
+    EditorModel.prototype.initTextArea = function() {
+      this.updateSize();
+      this.container.append("<div id=\"text\" contenteditable=\"true\" spellcheck=\"false\">" + this.config.welcome_text + "</div>");
+      return this.textarea = $("#text");
+    };
+
+    EditorModel.prototype.initInfoArea = function() {
+      this.updateInfo();
+      this.container.append("<div id=\"info\">\nWand /*\n<br>\nword count: " + this.word_count + "\n<br>\nmisspelled words:\n<br>\n" + (format_misspelling(this.misspelled)) + "\n</div>");
+      return this.info = $("#info");
+    };
+
+    EditorModel.prototype.updateInfo = function() {
+      this.word_count = countStr(this.text, this.config.word_regex);
+      return this.misspelled = check_spelling(this.text, this.config.word_regex, this.config.word_list);
+    };
+
+    EditorModel.prototype.drawInfo = function() {
+      return this.info.html("Wand /*\n<br>\nword count: " + this.word_count + "\n<br>\nmisspelled words:\n<br>\n" + (format_misspelling(this.misspelled)));
+    };
+
+    EditorModel.prototype.updateText = function() {
+      this.text = this.textarea.html();
+      this.updateInfo();
+      return this.drawInfo();
+    };
+
+    EditorModel.prototype.updateSize = function() {
+      var h, w;
+      h = $(window).innerHeight() / 20 * 19;
+      w = $(window).innerWidth() / 20 * 19;
+      this.container_height = h + "px";
+      return this.container_width = w + "px";
+    };
+
+    EditorModel.prototype.updateWindowSize = function() {
+      this.resize = true;
+      this.updateSize();
+      return this.view.update(this);
+    };
 
     return EditorModel;
 
@@ -46,11 +142,17 @@ you'll be able to find it at my github!
       if (this.config.debug) {
         console.log("+ Initiating EditorView");
       }
-      this.initiateTextArea(window.innterHeight, window.innerWidth);
     }
 
-    EditorView.prototype.initiateTextArea = function(width, height) {
-      return console.log();
+    EditorView.prototype.update = function(model) {
+      if (model.resize === true) {
+        if (this.config.debug) {
+          console.log("+ Resizing canvas");
+        }
+        model.container.css("height", model.container_height);
+        model.container.css("width", model.container_width);
+        return model.resize = false;
+      }
     };
 
     return EditorView;
@@ -58,13 +160,27 @@ you'll be able to find it at my github!
   })();
 
   EditorController = (function() {
-    function EditorController(config1, model) {
+    function EditorController(config1, model1) {
       this.config = config1;
-      this.model = model;
+      this.model = model1;
       if (this.config.debug) {
         console.log("+ Initiating EditorController");
       }
+      this.setupEvents();
     }
+
+    EditorController.prototype.setupEvents = function() {
+      $(window).resize((function(_this) {
+        return function() {
+          return _this.model.updateWindowSize();
+        };
+      })(this));
+      return this.model.container.on('keydown keyup focus', (function(_this) {
+        return function(event) {
+          return _this.model.updateText();
+        };
+      })(this));
+    };
 
     return EditorController;
 
--- spellcheck/src/main.coffee
+++ spellcheck/src/main.coffee
@@ -10,34 +10,133 @@ you'll be able to find it at my github!
 
 # Dependincies
 
-- jQuery, to make dom manipulations work across all browsers
+- jQuery, to make DOM manipulations work across all browsers
 
 ###
 
+countStr = (string, regex) ->
+  count = 0
+  for word in string.split(regex)
+    if word.length > 0
+      count += 1
+  return count
+
+format_misspelling = (l) ->
+  console.log(l)
+  s = ""
+  for word, other_spellings of l
+    s += "#{word}<br>"
+    for spelling in other_spellings
+      s += "? #{spelling}<br>"
+  return s
+
+check_spelling = (string, word_regex, word_list) ->
+  misspelled = {}
+  console.log string.split(word_regex)
+  for word in string.split(word_regex)
+    if word.length > 0 and word.toLowerCase() not in word_list
+      misspelled[word] = ['put reccomendations here']
+  return misspelled
 
 class Config
   constructor: ->
     @debug = true
-
+    @welcome_text = "Welcome to wand"
+    @word_regex = /[\ ,\.\!\;\?]|<br>/
+    @word_list = ["the", "and", "cat", "welcome", "to", "wand"]
 
 class EditorModel
   constructor: (@config, @view) ->
     console.log("+ Iniating Model") if @config.debug
-    @text = "Welcome to wand."
+    @text = @config.welcome_text
+
+    @initContainer()
+    @initTextArea()
+    @initInfoArea()
+    @updateWindowSize()
+
+  initContainer: ->
+    $('body').append '<div id="container"></div>'
+    @container = $('#container')
+
+  initTextArea: ->
+    @updateSize()
+    @container.append """
+    <div id="text" contenteditable="true" spellcheck="false">#{@config.welcome_text}</div>
+    """
+    @textarea = $("#text")
+
+  initInfoArea: ->
+    @updateInfo()
+    @container.append """
+    <div id="info">
+    Wand /*
+    <br>
+    word count: #{@word_count}
+    <br>
+    misspelled words:
+    <br>
+    #{format_misspelling(@misspelled)}
+    </div>
+    """
+    @info = $("#info")
+
+  updateInfo: ->
+    @word_count = countStr(@text, @config.word_regex)
+    @misspelled = check_spelling(@text, @config.word_regex, @config.word_list)
+
+  drawInfo: ->
+    @info.html """
+    Wand /*
+    <br>
+    word count: #{@word_count}
+    <br>
+    misspelled words:
+    <br>
+    #{format_misspelling(@misspelled)}
+    """
+
+  updateText: ->
+    @text = @textarea.html()
+    @updateInfo()
+    @drawInfo()
+
+  updateSize: ->
+    h = $(window).innerHeight() /20 * 19
+    w = $(window).innerWidth()  /20 * 19
+    @container_height = "#{h}px"
+    @container_width  = "#{w}px"
+
+  updateWindowSize: ->
+    @resize = true
+    @updateSize()
+    @view.update(@)
 
 
 class EditorView
   constructor: (@config) ->
     console.log("+ Initiating EditorView") if @config.debug
-    @initiateTextArea(window.innterHeight, window.innerWidth)
 
-  initiateTextArea: (width, height) ->
-    console.log()
+  update: (model) ->
+    if model.resize == true
+      console.log("+ Resizing canvas") if @config.debug
+      model.container.css("height", model.container_height)
+      model.container.css("width", model.container_width)
+      model.resize = false
+
 
 class EditorController
   constructor: (@config, @model) ->
     console.log("+ Initiating EditorController") if @config.debug
+    @setupEvents()
 
+  setupEvents: ->
+    $(window).resize( => # fat arrow notation means we can pass context too!
+      @model.updateWindowSize()
+    )
+    @model.container.on('keydown keyup focus', (event) =>
+      @model.updateText()
+    )
 
 config            = new Config()
 editor_view       = new EditorView(config)
