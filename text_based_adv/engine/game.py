653534b636a330ff7a2cf25e7652624eb92df8af
Aaron Delaney <devoxel@gmail.com>
2015-11-02 13:22:27 +0000

    Move world to seperate file

--- text_based_adv/engine/game.py
+++ text_based_adv/engine/game.py
@@ -4,12 +4,7 @@ File docs here, when they're ready!
 # Created by Aaron Delaney, see README.md for more information.
 
 import controller
-
-class World(object):
-    def __init__(self, assets):
-        self.debug = assets.meta.debug
-        self.assets = assets
-        self.current_area = assets.meta.places.starting_location
+import world
 
 class Game(object):
     def __init__(self, assets):
---
+++ text_based_adv/engine/world.py
@@ -0,0 +1,13 @@
+"""
+File docs here, when they're ready!
+"""
+# Created by Aaron Delaney, see README.md for more information.
+
+class World(object):
+    def __init__(self, assets):
+        self.debug = assets.meta.debug
+        self.assets = assets
+        self.current_area = assets.meta.places.starting_location"""
+        File docs here, when they're ready!
+        """
+        # Created by Aaron Delaney, see README.md for more information.
