41b3ea5efbfd9ec68dd78ee704f6e762ff5b3e51
Aaron Delaney <devoxel@gmail.com>
2015-11-02 13:23:08 +0000

    Remove misplaced commit message

--- text_based_adv/engine/world.py
+++ text_based_adv/engine/world.py
@@ -7,7 +7,4 @@ class World(object):
     def __init__(self, assets):
         self.debug = assets.meta.debug
         self.assets = assets
-        self.current_area = assets.meta.places.starting_location"""
-        File docs here, when they're ready!
-        """
-        # Created by Aaron Delaney, see README.md for more information.
+        self.current_area = assets.meta.places.starting_location
