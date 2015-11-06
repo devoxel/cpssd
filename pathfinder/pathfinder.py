f67284e710d76ab6b1c2ae14255188542c30ebad
Aaron Delaney <devoxel@gmail.com>
2015-11-06 10:56:33 +0000

    Polish up pathfinder

--- pathfinder/pathfinder.py
+++ pathfinder/pathfinder.py
@@ -65,16 +65,10 @@ Examples
     # Interactive mode
     $ python pathfinder.py --verbose
 
+Input Assumptions
+-----------------
 
-Interactive Mode
-----------------
-
-The input is much the same as the file input.
-
-Enter a string of characters for each row in the table and to signify
-then end of table just enter a newline.
-
-However, to save time for people entering many lines, you can enter
+To save time for people entering many lines, you can enter
 something like this:
 
 ```
@@ -88,17 +82,96 @@ And pathfinder will assume you mean:
 $ 1100 # <-- zeros were padded as an assumption
 $ 1111
 ```
+
+Interactive Mode
+----------------
+
+The input is much the same as the file input.
+
+Enter a string of characters for each row in the table and to signify
+then end of table just enter a newline.
+
 """
 
 import sys
 import traceback
 
+
+def num_of_paths(table, verbose=False):
+    """num_of_paths:
+    -> if !verbose: returns int which reflects amounts of paths
+    -> if  verbose: returns a tuple: (amount_of_paths, working table)
+
+    Finds the total possible of ways to traverse a table from the top left
+    to the bottom right.
+
+    The table can consist of 1s and 0s.
+
+    1s represent a movable location.
+    0s represent a location that is blocked off.
+
+    Table Input Format
+    [ [1,1,0,0], [1,1,1,1], [1,1,1,1], [1,1,1,1] ]
+
+    The table should be accessed by table[y][x]
+    """
+    # To get a better idea of how this algorithm functions, run the program
+    # with --verbose. It shows the working table.
+    try:
+        num_paths = 0
+        num_rows = len(table)
+        num_cols = len(table[0])
+        value_table = [[ 0 for j in range(0, num_cols) ] for i in range(0, num_rows)]
+        for cur_y in range(0, num_rows):
+            if len(table[cur_y]) != num_cols:
+                raise IndexError # we explicitly wants correct tables
+            for cur_x in range(0, num_cols):
+                value = table[cur_y][cur_x]
+                if value != 0 and value != 1:
+                    raise TypeError
+                if value == 1:  # tile is visitable
+                    left = cur_x - 1
+                    up   = cur_y - 1
+                    if cur_x == 0 and cur_y == 0:
+                        prev_left_value = 1
+                        prev_up_value = 0
+                    elif cur_x == 0:
+                        prev_left_value = 0
+                        prev_up_value = value_table[up][cur_x]
+                    elif cur_y == 0:
+                        prev_up_value = 0
+                        prev_left_value = value_table[cur_y][left]
+                    else:
+                        prev_up_value = value_table[up][cur_x]
+                        prev_left_value = value_table[cur_y][left]
+                    new_value = prev_left_value + prev_up_value
+                    value_table[cur_y][cur_x] = new_value
+        if verbose:
+            return value_table[num_rows-1][num_cols-1], value_table
+        else:
+            return value_table[num_rows-1][num_cols-1]
+    except IndexError:
+        if verbose:
+            print '\nInvalid table, eg: unbalanced rows'
+            print traceback.format_exc()
+        return -1
+    except TypeError:
+        if verbose:
+            print traceback.format_exc()
+            print '\nPassed in the wrong data'
+        return -1
+
 def _formatted_table(t):
-    """Little helper function to print a nicly formatted table"""
-    # if t looks like [[12,19,3,4],[2,6,100,1]]
-    # output should be:
-    #     12 19 3   4
-    #     2  6  100 1
+    """A helper function to print a nice looking table.
+
+    If t looks like [[12,19,3,4],[2,6,100,1]]
+    -> output should be:
+        |012|019|003|004
+        |002|006|100|001
+
+    On failure, just return a string of t
+    """
+
     try:
         digit_padding = 0
         for row in t:
@@ -107,18 +180,21 @@ def _formatted_table(t):
         out = ""
         for row in t:
             out += '|'
+            row_string = ''
             for v in row:
                 s = str(v)
                 padding = (digit_padding - len(s))
-                out += s + ' '*padding + '|'
-            out += '\n'
+                row_string += '0'*padding + s +  '|'
+            # 79 here refers to the standard terminal size in most OS's
+            if len(row_string) > 79:
+                return 'Too big!'
+            out +=  row_string + '\n'
     except:
         out = str(t)
     return out
 
 def pathfinder_cli():
     """CLI interprets the arguments and options passed to pathfinder.py"""
-
     # In order to allow verbose to be the last input the evaluations
     # must be carried out after reading all the arguments.
     verbose = False
@@ -164,150 +240,95 @@ def parse_binary_string(s):
             binary_list.append(int(c))
     return binary_list
 
-def handle_table_input(string, verbose):
-    table = []
-    output = []
-    for row in string.split('\n'): 
-        current_row = parse_binary_string(row)
-        print current_row
-        if len(current_row) > 0:
-            table.append(current_row)
+def pad_table(table, columns):
+    for row in table:
+        if len(row) < columns:
+            for i in range(len(row), columns):
+                row.append(0) # tables are mutable, so no need to return
+
+def parse_input(user_input):
+    tables = []
+    current_table = []
+    max_column = 0 # try to fix user input#
+
+    for input_row in user_input:
+        if len(input_row) > 0:
+            real_input = parse_binary_string(input_row)
+            max_column = max(len(real_input), max_column)
+            current_table.append(real_input)
         else:
-            path_output = '\nNumber of paths: ' + str(num_of_paths(table, verbose))
-            if verbose:
-                output.append( _formatted_table(table) + path_output )
-            else:
-                output.append(path_output)
-    return output
+            if len(current_table) > 0:
+                pad_table(current_table, max_column)
+                tables.append(current_table)
+                current_table = []
+                max_column = 0
+
+    if len(tables) > 0:
+        return tables
+    else:
+        pad_table(current_table, max_column)
+        return current_table
+
+def get_output(table, verbose):
+    number_of_paths = num_of_paths(table, verbose)
+    path_info = '\nNumber of paths: '
+    if verbose:
+        paths = number_of_paths[0]
+        working_table = number_of_paths[1]
+        output_str = '\nTable\n' + _formatted_table(table)
+        output_str += '\nWorking Table\n' + _formatted_table(working_table)
+        output_str += path_info + str(paths)
+        return output_str
+    else:
+        return path_info + str(number_of_paths)
 
 def parse_file(filepath, verbose):
     file_string = ''
     with open(filepath) as f:
-        file_string = f.read()    
-    for output in handle_table_input(file_string, verbose):
-        print output 
+        file_string = f.read()
+    tables = parse_input(file_string.split('\n'))
+    for table in tables:
+        print get_output(table, verbose)
 
 def interactive_input(verbose):
     """Take user input from strings"""
     # this syntax is a special multiline string
     prompt = ("\nEnter your table by simple writing a 0 for an unpassable tile\n"
               "or a 1 for an unpassable tile, as a string of integers\n\n"
-              "For example:\n11100111\n\n"
               "Press CTRL+C or type s to exit\n"
               "Blank line runs pathfinder on entered table\n"
               "Any unexpected value is interpreted as a newline\n"
              )
-    string_seperator = "_________________________________"
+    string_seperator = "_" * 20
 
     while True:
-        table_entered = False
-        table = []
-        max_columns = 0
         print string_seperator
         print prompt
+
+        table_entered = False
+        current_input = [] # Input seperated by rows
         while not table_entered:
-
-            row = []
             user_input = raw_input()
-
             if user_input[0:1].lower() == 's':
                 sys.exit(0)
 
-            try:
-                user_input = parse_binary_string(user_input)
-                for c in user_input:
-                    row.append(int(c))
-            except ValueError:
-                print 'Invalid character in row'
-                if verbose: print traceback.format_exc()
-                continue # don't evaluate the rest of this while loop
-
-            max_columns = max(len(row), max_columns)
-
-            if len(row) == 0:
+            if len(user_input) == 0:
                 table_entered = True
             else:
-                table.append(row)
+                current_input.append(user_input)
 
-        for row in table:
-            if len(row) < max_columns:
-                for i in range(0, max_columns-len(row)):
-                    # lists are mutable types so this will change table too
-                    row.append(0)
+        table = parse_input(current_input)
+        print get_output(table, verbose)
 
-        print handle_table_input(table, verbose)[0]
         print string_seperator
         user_input = raw_input('\nEnter "s" to stop, otherwise continue: ')
         if user_input[0:1].lower() == 's':
             sys.exit(0)
 
-def num_of_paths(table, verbose, num_rows=None, num_cols=None):
-    """num_of_paths -> returns int
-
-    Finds the total possible of ways to traverse a table from the top left
-    to the bottom right.
-
-    The table can consist of 1s and 0s.
-
-    1s represent a movable location.
-    0s represent a location that is blocked off.
-
-    Table Input Format
-     [
-       [1,1,0,0],
-       [1,1,1,1],
-       [1,1,1,1],
-       [1,1,1,1]
-     ]
-    The table should be accessed by table[y][x]
-    """
-    try:
-        num_paths = 0
-        if num_rows == None: num_rows = len(table)
-        if num_cols == None: num_cols = len(table[0])
-        value_table = [[ 0 for j in range(0, num_cols) ] for i in range(0, num_rows)]
-        for cur_y in range(0, num_rows):
-            if len(table[cur_y]) != num_cols:
-                raise IndexError # we explicitly wants correct tables
-            for cur_x in range(0, num_cols):
-                value = table[cur_y][cur_x]
-                if value != 0 and value != 1:
-                    raise TypeError
-                if value == 1:  # tile is visitable
-                    left = cur_x - 1
-                    up   = cur_y - 1
-                    if cur_x == 0 and cur_y == 0:
-                        prev_left_value = 1
-                        prev_up_value = 0
-                    elif cur_x == 0:
-                        prev_left_value = 0
-                        prev_up_value = value_table[up][cur_x]
-                    elif cur_y == 0:
-                        prev_up_value = 0
-                        prev_left_value = value_table[cur_y][left]
-                    else:
-                        prev_up_value = value_table[up][cur_x]
-                        prev_left_value = value_table[cur_y][left]
-                    new_value = prev_left_value + prev_up_value
-                    value_table[cur_y][cur_x] = new_value
-        if verbose:
-            print _formatted_table(value_table)
-        return value_table[num_rows-1][num_cols-1]
-    except IndexError:
-        if verbose:
-            print '\nInvalid table, eg: unbalanced rows'
-            print traceback.format_exc()
-        return -1
-    except TypeError:
-        if verbose:
-            print traceback.format_exc()
-            print '\nPassed in the wrong data'
-        return -1
-
 def test_num_of_paths(verbose):
-    """_test(verbose): run unit tests on num_of_paths
+    """test_num_of_paths(verbose): run unit tests on num_of_paths
 
-    If verbose is true lots of stuff is printed
+    If verbose is true, LOTS of stuff is printed about the tests
     """
     if verbose:
         print '\n# Testing pathfinder.py'
@@ -332,7 +353,7 @@ def test_num_of_paths(verbose):
         expected_value  = test_data[key][1]
         # verbose is false because we don't need to see things
         # multiple tinmes
-        value           = num_of_paths(list_to_test, verbose=False)
+        value = num_of_paths(list_to_test)
         if verbose:
             print '-', 'testing:\n', _formatted_table(list_to_test)
             print '-', 'should return ', expected_value
@@ -342,7 +363,7 @@ def test_num_of_paths(verbose):
         except AssertionError:
             if verbose: print traceback.format_exc()
             print '++ Test failed. Run with --verbose'
-            sys.exit(1)
+            sys.exit(3)
 
     print '++ Tests passed'
     return 0
