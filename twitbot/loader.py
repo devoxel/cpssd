ef6f76a1edcab463bedd2cb868e3710a057473ac
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2015-11-17 02:57:10 +0000

    Add some more basic code to twitter bot

--- twitbot/loader.py
+++ twitbot/loader.py
@@ -3,11 +3,10 @@ sys.dont_write_bytecode = True
 from src.markov import MarkovChain, parse_corpus
 
 if __name__ == "__main__":
-    mrk = MarkovChain()
     with open("corpuses/training.1600000.processed.noemoticon.csv", "r") as f:
-        corpus = parse_corpus(f, 1000)
-    mrk.build_chain(corpus)
+        corpus = parse_corpus(f, 10000)
+    mrk = MarkovChain(corpus)
 
     while True:
         print mrk.generate()
-        raw_input("... enter to generate more")
+        raw_input("\nEnter to generate more, ctrl+z to exit\n... ")
--- twitbot/src/markov.py
+++ twitbot/src/markov.py
@@ -20,42 +20,58 @@ def parse_corpus(csvf, lines):
 
 
 class WordPair(object):
-    def __init__(self, words):
-        self._len = 1
-        self._words = words
+    def __init__(self, *args):
+        self._words = []
+        self._str = ""
+        for word in args:
+            if word is not None:
+                self._words.append(clean_up(word))
+                self._str += " " + word
 
-    def add(self, s):
-        self._len += 1
+        if self._words > 0:
+            self._words = tuple(self._words)
+        else:
+            self._words = None
+
+        self._len = 1
+
+    def __repr__(self):
+        return repr(self._words) + '*' + repr(self._len)
 
     def __len__(self):
         return self._len
 
+    def __eq__(self, other):
+        return self._words == other._words
+
     def __hash__(self):
         return hash(self._words)
 
+    def __str__(self):
+        return self._str
 
 class Prefix(WordPair):
     pass
 
-
 class Suffix(WordPair):
-    pass
+    def add(self):
+        self._len += 1
 
 
 class MarkovChain(object):
     def __init__(self, corpus):
         """ MarkovChain(corpus) -> returns MarkovChain built with corpus
-        
-        Arguments         
-        ---------                
+
+        Arguments
+        ---------
         #### corpus
             type: list of strings
-        
+
         Chain Storage
         -------------
-        The Markov Chain is stored in a large dictionary where 
+        The Markov Chain is stored in a large dictionary where
         Prefix objects map to Suffix objects.
-        
+
         These objects are based on the Words object which handles
         the overrides. The object also handles removing punctuation
         and handling probability of each word being a starting word,
@@ -69,52 +85,52 @@ class MarkovChain(object):
         """
         """
         for i in xrange(0, len(corpus)):
-            prefix = Prefix(safe_get(corpus[i:i+1], 0) ,
-                            safe_get(corpus[i+1:i+2], 0))
-            suffix = Suffix(safe_get(corpus[i+3:i+4], 0),
-                            safe_get(corpus[i+4:i+5], 0))
-            if prefix is not None and suffix is not None:
-                clean_prefix = clean_up(prefix)
-                clean_suffix = clean_up(suffix)
-                
-                if clean_prefix not in self._prefixs:
-                    self._prefixs[clean_prefix] = Suffix(prefix)
-                else:
-                    self._prefixs[clean_prefix].add(prefix)
+            word1 = safe_get(corpus[i:i+1], 0)
+            word2 = safe_get(corpus[i+1:i+2], 0)
+            word3 = safe_get(corpus[i+2:i+3], 0)
+            word4 = safe_get(corpus[i+4:i+5], 0)
 
-                prefix = clean_up(prefix)
-                if prefix not in self._chain:
-                    self._chain[prefix] = { clean_suffix: Suffix(suffix) }
-                elif prefix in self._chain:
-                    if clean_suffix in self._chain[prefix]:
-                        self._chain[prefix][clean_suffix].add(suffix)
-                    else:
-                        self._chain[prefix][clean_suffix] = Suffix(suffix)
+            prefix = Prefix(word1, word2)
+            suffix = Suffix(word3, word4)
+            mapped_suffixs = self._chain.get(prefix)
+            if mapped_suffixs is None:
+                self._chain[prefix] = [suffix]
+            else:
+                if suffix in mapped_suffixs:
+                    mapped_suffixs[mapped_suffixs.index(suffix)].add()
+                else:
+                    mapped_suffixs.append(suffix)
 
     def generate(self):
-        starts = random.sample(self._prefixs, 25)
-        sorted_starts = sorted( self._prefixs.keys(), 
-                        key=lambda x: self._prefixs[x].starting_word, 
-                        reverse=True )
+        possible_starts = random.sample(self._chain.keys(), 50)
+
+        sorted_starts = sorted( possible_starts,
+                                key=lambda x: len(self._chain[x]) )
+        accum = 1 - abs(random.gauss(0, .25))
+        while accum
+
         s = ""
         limit = 140
-        while len(s) + len(current) < limit:
-            s += current + " "
-            sorted_suffixs = sorted( self._chain[current].keys(), 
-                                key= lambda x: len(self._chain[current][x]),
-                                reverse=True )
-            accum = random.random()
-            changed = False
-            first_possible = None
-            for key in sorted_suffixs:
-                if key in self._chain:
-                    if first_possible is None:
-                        first_possible = key
-                    cost = len(self._chain[current][key])/len(sorted_suffixs)
-                    accum += cost
-                    if accum >= 1:
-                        current = key
-                        changed = True
-            if not changed:
-                current = first_possible
-        return s
+        current_prefix = sorted_starts.pop()
+        while len(s) < limit:
+            s += str(current_prefix)
+            sorted_suffixs = sorted( self._chain[current_prefix], reverse=True,
+                                     key=lambda x: len(x) )
+            print current_prefix
+            print sorted_suffixs
+            # work on the probabilities like accumulators
+            accum = 1
+            chosen = False
+            i = 0
+            while accum > 0:
+                if suffix in self._chain:
+                    cost = len(sorted_suffixs[i]) // len(sorted_suffixs)
+                    accum -= cost
+                i += 1
+
+            if not chosen and len(other_possibilities) > 0:
+                current_prefix = other_possibilities.pop()
+            else:
+                break
+
+        return s + "."
